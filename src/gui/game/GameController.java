b6488add654223da77c142336b8788473565e26b
Alex Kir <alexvoorfontys@gmail.com>
2017-10-21 12:37:53 +0200

    few changes with GUI of game

--- src/gui/game/GameController.java
+++ src/gui/game/GameController.java
@@ -36,14 +36,14 @@ public class GameController {
     private Integer points = 0;
 
     public GameController() {
-        player1 = new Player(50, 600, Direction.DOWN, 1, 0);
+        player1 = new Player(50, 600, Direction.UP, 1, 0);
         player2 = new Player(950, 600, Direction.UP, 2, 0);
         animationTimer = new AnimationTimer() {
             @Override
             public void handle(long now) {
                 updateCanvas();
                 //TODO maybe new timer for intersection?
-                if (player1.intersects(player2) || player2.intersects(player1) || player2.hitsGrid())
+                if (player1.intersects(player2) || player2.intersects(player1) ||  player1.hitsGrid() || player2.hitsGrid())
                 {
                     animationTimer.stop();
                     playerTimer.cancel();
