b5d3993cb3ddfa154a60ec29b1af4aa3a26bc4b3
Alex Kir <alexvoorfontys@gmail.com>
2017-10-24 10:45:58 +0200

    Unittests finished for Room repo

--- tests/Game/IRoomRepositoryUnitTests.java
+++ tests/Game/IRoomRepositoryUnitTests.java
@@ -1,12 +1,15 @@
 package Game;
 import classes.domains.JavaFXPaintable;
+import classes.domains.Room;
 import classes.domains.User;
 import classes.repositories.IRoomRepository;
 import classes.repositories.RoomRepository;
 import gui.game.GameController;
 import javafx.scene.canvas.Canvas;
+import org.junit.Assert;
 import org.junit.Test;
 
+import javax.jws.soap.SOAPBinding;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -15,9 +18,44 @@ public class IRoomRepositoryUnitTests {
 
     @Test
     public void testConstructor() throws SQLException, IOException, ClassNotFoundException {
+        boolean passed = true;
+        try{
+            IRoomRepository roomRepository = new RoomRepository();
+        }
+        catch (Exception e)
+        {
+            passed = false;
+        }
+        Assert.assertEquals(true,passed);
 
-        IRoomRepository roomRepository = new RoomRepository();
-        List<User> users = roomRepository.getLobby(30);
-        org.junit.Assert.assertEquals(1,1);
+    }
+    @Test
+    public void testCreateRoom() throws SQLException, IOException, ClassNotFoundException {
+        boolean passed = true;
+        try{
+            IRoomRepository roomRepository = new RoomRepository();
+            roomRepository.createRoom(1,"bob","jan",50);
+
+        }
+        catch (Exception e)
+        {
+            passed = false;
+        }
+        Assert.assertEquals(true,passed);
+
+    }
+    @Test
+    public void testgetLobby() throws SQLException, IOException, ClassNotFoundException {
+        boolean passed = true;
+        try{
+            IRoomRepository roomRepository = new RoomRepository();
+            List<User> users = roomRepository.getLobby(30);
+        }
+        catch (Exception e)
+        {
+            passed = false;
+        }
+        Assert.assertEquals(true,passed);
+
     }
 }
