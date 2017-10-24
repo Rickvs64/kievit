aec979288b1eed31b28b22bc9e24788818b1f448
Alex Kir <alexvoorfontys@gmail.com>
2017-10-24 11:01:00 +0200

    UnitTest for room updated

--- tests/Game/RoomUnitTests.java
+++ tests/Game/RoomUnitTests.java
@@ -2,14 +2,17 @@ package Game;
 
 import classes.domains.Room;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class RoomUnitTests {
-    @Test
+    Room room;
+    @Before
     public void testConstructor()  {
         boolean passed = true;
         try{
-            Room room = new Room(0,"bob","Jan",false);
+            room = new Room(0,"bob","Jan",false);
         }
         catch (Exception e)
         {
@@ -20,36 +23,24 @@ public class RoomUnitTests {
     }
     @Test
     public void testID()  {
-        Room room = new Room(0,"bob","Jan",false);
         room.setID(2);
-        int ID = room.getID();
-
-        Assert.assertEquals(2,ID);
+        Assert.assertEquals(2,room.getID());
 
     }
     @Test
     public void testName()  {
-        Room room = new Room(0,"bob","Jan",false);
         room.setName("Karel");
-        String name = room.getName();
-
-        Assert.assertEquals("Karel",name);
+        Assert.assertEquals("Karel",room.getName());
     }
     @Test
     public void testPass()  {
-        Room room = new Room(0,"bob","Jan",false);
         room.setPassword("1234");
-        String pass = room.getPassword();
-
-        Assert.assertEquals("1234",pass);
+        Assert.assertEquals("1234",room.getPassword());
 
     }
     @Test
     public void testStarted()  {
-        Room room = new Room(0,"bob","Jan",false);
         room.setStarted(true);
-
-
         Assert.assertEquals(true,room.getStarted());
 
     }
