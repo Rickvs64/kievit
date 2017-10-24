8773877ddab4a901bf6ae63b35efb556554efab9
Lars Janssen <larsjanssen64@gmail.com>
2017-10-24 09:44:06 +0200

    Room redirect fix

--- src/gui/home/HomeController.java
+++ src/gui/home/HomeController.java
@@ -1,6 +1,7 @@
 package gui.home;
 
 import classes.domains.User;
+import gui.room.create.RoomController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -30,20 +31,20 @@ public class HomeController {
     }
 
     @FXML
-    private void openLobby() throws IOException {
+    private void openRoom() throws IOException {
         // Set the next "page" (scene) to display.
         // Note that an incorrect path will result in unexpected NullPointer exceptions!
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../room/lobby/lobby.fxml"));
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../room/create/Room.fxml"));
         Parent root = (Parent)fxmlLoader.load();
-
-
-        Scene lobby = new Scene(root);
-
+        RoomController roomController = fxmlLoader.<RoomController>getController();
+        // Run the setUser() method in HomeController.
+        // This is the JavaFX equivalent of sending data from one form to another in C#.
+        roomController.setUser(user);
+        Scene homeScreen = new Scene(root);
         Stage stage;
         stage = (Stage) lbl_username.getScene().getWindow(); // Weird backwards logic trick to get the current scene window.
 
-        stage.setScene(lobby);
+        stage.setScene(homeScreen);
         stage.show();
     }
 }
