deef02cd03b370a7e3fbfb60d8714a777c938625
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-11-14 13:15:24 +0100

    make intersection method more robust

--- src/classes/domains/Player.java
+++ src/classes/domains/Player.java
@@ -3,6 +3,7 @@ package classes.domains;
 
 import javafx.scene.image.Image;
 
+import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -82,23 +83,39 @@ public class Player {
     }
 
     public boolean intersects(Player other) {
+        if (coordinates.size() <= 1)
+            return false;
+        if (other.getCoordinates().size() <= 1)
+            return false;
         Coordinate head = coordinates.get(0);
-        for (Coordinate c:other.coordinates
-                ) {
-            if (c.getY() == head.getY() && c.getX() == head.getX())
-            {
+        Coordinate previous = coordinates.get(1);
+
+        Line2D line = new Line2D.Float(head.getX(), head.getY(), previous.getX(), previous.getY());
+
+        Coordinate previousOther = other.getCoordinates().get(0);
+        for (Coordinate c : other.getCoordinates()) {
+            Line2D LineOther = new Line2D.Float(c.getX(), c.getY(), previousOther.getX(), previousOther.getY());
+            if (line.intersectsLine(LineOther))
                 return true;
-            }
+
+            previousOther = c;
         }
 
-        List<Coordinate> temp = coordinates.stream().collect(Collectors.toList());
-        temp.remove(0);
-        for (Coordinate c2: temp) {
-            if (c2.getY() == head.getY() && c2.getX() == head.getX())
-            {
-                return true;
+        List<Coordinate> coordinates1 = getCoordinates();
+        for (int i = 0, coordinates1Size = coordinates1.size(); i < coordinates1Size; i++) {
+            Coordinate c = coordinates1.get(i);
+            if (i <= 2) {
+                previous = c;
+                continue;
             }
+
+            Line2D LineOther = new Line2D.Float(c.getX(), c.getY(), previous.getX(), previous.getY());
+            if (line.intersectsLine(LineOther))
+                return true;
+            
+            previous = c;
         }
+
         return false;
     }
 
@@ -106,20 +123,13 @@ public class Player {
     {
         Coordinate head = coordinates.get(0);
 
-        if(
-            //Check top
-            (head.getX() >= 10 & head.getX() <= 970) & (head.getY() <= 18) ||
-            //Check bottom
-            (head.getX() >= 10 & head.getX() <= 970) & (head.getY() >= 743) ||
+        return (head.getX() >= 10 & head.getX() <= 970) & (head.getY() <= 18) ||
+                //Check bottom
+                (head.getX() >= 10 & head.getX() <= 970) & (head.getY() >= 743) ||
+                //Check left
+                (head.getY() >= 10 & head.getY() <= 760) & (head.getX() <= 10) ||
+                //Check right
+                (head.getY() >= 10 & head.getY() <= 760) & (head.getX() >= 970);
 
-            //Check left
-            (head.getY() >= 10 & head.getY() <= 760) & (head.getX() <= 10) ||
-            //Check right
-            (head.getY() >= 10 & head.getY() <= 760) & (head.getX() >= 970))
-        {
-            return true;
-        }
-
-        return false;
     }
 }
