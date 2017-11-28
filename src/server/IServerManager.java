8003576944b0ebf1c118dce595cfdd4c7ec0f173
Alex Kir <alexvoorfontys@gmail.com>
2017-11-28 18:06:20 +0100

    game RMI few fixes

--- src/server/IServerManager.java
+++ src/server/IServerManager.java
@@ -15,5 +15,5 @@ public interface IServerManager extends Remote {
     boolean getStatus(int id) throws RemoteException;
     void setStatus(boolean b,int id) throws RemoteException;
     void updateDirection(Direction currentDirection, int id, int userID) throws RemoteException;
-    Direction getDirection(int userID, int id) throws  RemoteException;;
+    Direction getDirection(int userID, int id) throws  RemoteException;
 }
